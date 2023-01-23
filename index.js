2e51b8d4c55c71c2e73214cfc7b4a072007ab6ec
Alejandrolaresss <121467143+Alejandrolaresss@users.noreply.github.com>
2023-01-22 23:45:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function () {
+    document.getElementById("info").innerHTML = "Proin a sem id lacus condimentum porttitor. In efficitur nisl id risus pretium, pretium commodo felis dignissim. Curabitur vel pellentesque orci, ullamcorper aliquet erat. Cras in ex dolor. Proin consequat egestas rhoncus. Etiam eget luctus nisl, eget porttitor arcu. Aliquam quam nibh, hendrerit in massa non, maximus euismod sapien. Sed odio lacus, convallis in viverra in, cursus ac libero. Nulla feugiat lorem vel molestie ullamcorper. Sed tincidunt eu nunc vitae blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam scelerisque lectus urna.";
+}
+
